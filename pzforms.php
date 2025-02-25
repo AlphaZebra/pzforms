f555a72070af4b40693993db1ca2ad0c192c3136
Robert Richardson <robert@peakzebra.com>
2025-02-25 09:58:43 -0500

    fixed version number

--- pzforms.php
+++ pzforms.php
@@ -4,7 +4,7 @@
  * Description:       Standalone block-based form builder.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            PeakZebra / Robert Richardson
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
